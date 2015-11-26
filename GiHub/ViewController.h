df780ae7c42dcd9cacac0ccddd563307e264a914
Sundeep Manne <sundeepmanne@rocketmail.com>
2015-11-26 10:31:12 +0530

    Initial Commit

---
+++ GiHub/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GiHub
+//
+//  Created by SUNDEEP MANNE on 26/11/15.
+//  Copyright © 2015 Adeptpros. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GiHub/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  GiHub
+//
+//  Created by SUNDEEP MANNE on 26/11/15.
+//  Copyright © 2015 Adeptpros. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
